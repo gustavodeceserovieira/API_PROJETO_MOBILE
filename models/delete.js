0c085c63b754ea4f2a1e238318dfe3eb64d38013
Gustavo <gustavodeceserovieira@gmail.com>
2026-03-13 22:19:20 -0300

    Correção do nome da variável de conexão com o banco

--- models/delete.js
+++ models/delete.js
@@ -5,14 +5,14 @@ export async function deleta_aluno_historico(rg_aluno){
   return rows
 }
 export async function deleta_aluno(rg_aluno){
-  const[rows] = await db.execute('DELETE FROM aluno WHERE rg_aluno=?',[rg_aluno])
+  const[rows] = await pool.execute('DELETE FROM aluno WHERE rg_aluno=?',[rg_aluno])
   return rows
 }
 export async function deleta_responsaveis_aluno(rg_aluno){
-  const[rows] = await db.execute('DELETE FROM responsaveis WHERE rg_aluno=?',[rg_aluno])
+  const[rows] = await pool.execute('DELETE FROM responsaveis WHERE rg_aluno=?',[rg_aluno])
   return rows
 }
 export async function deleta_presenca_aluno(rg_aluno){
-  const[rows] = await db.execute('DELETE FROM presenca WHERE rg_aluno=?',[rg_aluno])
+  const[rows] = await pool.execute('DELETE FROM presenca WHERE rg_aluno=?',[rg_aluno])
   return rows
 }
